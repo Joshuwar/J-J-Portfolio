43277d7e68993fdd43f04dbba32f0d00a845e9f6
Jonathan Lister <jonathan@withjandj.com>
2011-07-10 21:00:20 +0100

    only showing testimonial and collaborators if they exist

--- JJ-theme/meta.php
+++ JJ-theme/meta.php
@@ -1,17 +1,24 @@
 <?php if ($post->post_type == "portfolio_items") : // Do the portfolio meta (if appropriate) ?>
 						<div class="meta">
+							<?php
+								$collaborators = get_post_meta($post->ID,'_collaborators',true);
+								$testimonial = get_post_meta($post->ID,'_testimonial',true);
+								$testimonial_name = get_post_meta($post->ID,'_testimonial_name',true);
+							?>
 							<div>
 								<h6><a href="http://<?php get_custom_field('_url'); ?>">Launch Site</a></h6>
 								<br/>
+								<?php if($collaborators) :?>
 								<h6>J&amp;J worked with:</h6>
-								<p><?php get_custom_field('_collaborators'); ?></p>
+								<p><?php echo $collaborators; ?></p>
+								<?php endif; ?>
 							</div>
 							<div class="marginleft">
+								<?php if($testimonial) :?>
 								<h6>Testimonial</h6>
-
-									<p><em><?php get_custom_field('_testimonial'); ?></em></p>
-									<span>- <?php get_custom_field('_testimonial_name'); ?></span>
-
+									<p><em><?php echo $testimonial; ?></em></p>
+									<span>- <?php echo $testimonial_name; ?></span>
+								<?php endif; ?>
 							</div>
 						</div>
 						<a href="<?php echo get_permalink_by_name('super-contact'); ?>">
